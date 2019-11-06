40178ba2b5ab16187fbffbbe3fd906c44a8842d2
Jeremy <Jrmyrosenberg@gmail.com>
2019-11-06 11:21:59 -0800

    working sign up and log in form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,23 @@
 import GreetingContainer from "./greeting/greeting_container";
 import React from "react";
+import SignupFormContainer from './session_form/signup_form_container'
+import LoginFormContainer from './session_form/login_form_container'
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
 
 const App = () => (
     <div>
+        <header>
         <h1>Sincere Eats</h1>
         <GreetingContainer />
+        </header>
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
