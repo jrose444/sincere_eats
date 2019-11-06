1515b182136f564c6cd71dc325f5e062606e69ac
Jeremy <Jrmyrosenberg@gmail.com>
2019-11-05 17:37:07 -0800

    got stuff on the page

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import GreetingContainer from "./greeting/greeting_container";
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>Sincere Eats</h1>
+        <GreetingContainer />
+    </div>
+);
+
+export default App;
--- frontend/serious_eats.jsx
+++ frontend/serious_eats.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Root from './components/root'
 import ReactDOM from "react-dom";
 // import { signup, login, logout } from './util/session_api_util';
 import {signup, login, logout} from './actions/session_actions'
@@ -7,10 +8,13 @@ import configureStore from './store/store'
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
-    window.login = login;
-    window.signup = signup;
-    window.logout = logout;
+
     window.getState = store.getState;
-    window.dispatch = store.dispatch; 
-    ReactDOM.render(<h1>Welcome to Serious Eats</h1>, root);
+    window.dispatch = store.dispatch;
+    window.login = login;
+    // window.signup = signup;
+    // window.logout = logout;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch; 
+    ReactDOM.render(<Root store = {store} />, root);
 });
