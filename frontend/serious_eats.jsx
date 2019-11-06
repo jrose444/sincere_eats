1c8b407f33213572300bb478a09ce819809d8a75
Jeremy <Jrmyrosenberg@gmail.com>
2019-11-05 16:53:37 -0800

    create working dispatch

--- frontend/serious_eats.jsx
+++ frontend/serious_eats.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { signup, login, logout } from './util/session_api_util';
+// import { signup, login, logout } from './util/session_api_util';
+import {signup, login, logout} from './actions/session_actions'
 import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
-    // window.login = login;
-    // window.signup = signup;
-    // window.logout = logout;
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
     window.getState = store.getState;
     window.dispatch = store.dispatch; 
     ReactDOM.render(<h1>Welcome to Serious Eats</h1>, root);
