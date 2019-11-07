21983525febfa4eb7f913a1eac18e389c19734ef
Jeremy <Jrmyrosenberg@gmail.com>
2019-11-06 16:10:59 -0800

    establish navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import GreetingContainer from "./greeting/greeting_container";
 import React from "react";
 import SignupFormContainer from './session_form/signup_form_container'
 import LoginFormContainer from './session_form/login_form_container'
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import {
     Route,
     Redirect,
@@ -12,12 +13,12 @@ import {
 
 const App = () => (
     <div>
-        <header>
+        <header className="the-navbar">
         <h1>Sincere Eats</h1>
         <GreetingContainer />
         </header>
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </div>
 );
 
